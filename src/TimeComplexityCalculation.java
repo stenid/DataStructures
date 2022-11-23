2642da6aa89b7ff83ee83d1daefaddcee9f2b396
stenid <stenid8@gmail.com>
2022-11-23 17:05:38 +0700

    Time complexity Calculation. From video # 12

--- src/TimeComplexityCalculation.java
+++ src/TimeComplexityCalculation.java
@@ -12,11 +12,11 @@ public class TimeComplexityCalculation {
 // In third line time complexity equals to 1. It takes constant amount of time.
 
 // For first for loop(4th line):
-//      Iter 1      i = n/2 + 0
-//      Iter 2      i = n/2 + 1
-//      Iter 3      i = n/2 + 2
-//      Iter 4      i = n/2 + 3
-//      Iter p      i = n/2 + p-1 = n // it equals to n because in last iteration
+//      Iteration 1      i = n/2 + 0
+//      Iteration 2      i = n/2 + 1
+//      Iteration 3      i = n/2 + 2
+//      Iteration 4      i = n/2 + 3
+//      Iteration p      i = n/2 + p-1 = n // it equals to n because in last iteration
 // we got condition i <= n in for loop.
 //      n/2 + p - 1 = n
 //      p = 1+n-n/2
@@ -24,22 +24,22 @@ public class TimeComplexityCalculation {
 //      loop executes n/2 times. so we can take the least upper bound as 'n'
 
 // For second loop(5th line):
-//      iter 1      j = 1 = 2^0
-//      iter 2      j = 2 = 2^1
-//      iter 3      j = 4 = 2^2
-//      iter 4      j = 8 = 2^3
-//      iter p      j = 2^(p-1) = n (because of condition of loop)
+//      iteration 1      j = 1 = 2^0
+//      iteration 2      j = 2 = 2^1
+//      iteration 3      j = 4 = 2^2
+//      iteration 4      j = 8 = 2^3
+//      iteration p      j = 2^(p-1) = n (because of condition of loop)
 //      n = 2^(p-1)
 //      log2(n) = p-1
 //      p = log2(n) + 1
 //      Time complexity of this loop is O(log2(n))
 
 // For third loop(6th line)"for(k = 1; k <= n; k++)"
-//      iter 1      k = 1
-//      iter 2      k = 2
-//      iter 3      k = 3
-//      iter 4      k = 4
-//      iter p      k = p = n (condition of loop)
+//      iteration 1      k = 1
+//      iteration 2      k = 2
+//      iteration 3      k = 3
+//      iteration 4      k = 4
+//      iteration p      k = p = n (condition of loop)
 //      Time complexity of that loop is O(n)
 
 // Overall time complexity equals to multiplication of all previous big O, because
